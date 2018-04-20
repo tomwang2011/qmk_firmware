610a0c5d38518282bc98529abb11d7ba13bfcc92
tom <tomwang2011@gmail.com>
2018-04-19 19:10:34 -0700

    commit

--- keyboards/xd75/keymaps/tw/keymap.c
+++ keyboards/xd75/keymaps/tw/keymap.c
@@ -1,4 +1,4 @@
-=/* Copyright 2017 Wunder
+/* Copyright 2017 Tom
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,23 +25,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | `      | -      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | \      | [      | ]      | Y      | U      | I      | O      | P      |   \    |
+ * | TAB    | Q      | W      | E      | R      | T      |    ~   | [      | ]      | Y      | U      | I      | O      | P      |   \    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CTRL   | A      | S      | D      | F      | G      | HOME   | PG UP  | "      | H      | J      | K      | L      | ;      | ENTER  |
+ * | CTRL   | A      | S      | D      | F      | G      | HOME   | PG UP  | ENTER  | H      | J      | K      | L      | ;      | "      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | END    | PG DN  | DEL    | N      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | END    | PG DN  | DEL    | N      | M      | ,      | .      | /      | UP     |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | TGFN   | SPACE  | SPACE  | RALT   | RCTRL  | LEFT   | DOWN   | UP     | RIGHT  |
+ * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | TGFN   | SPACE  | SPACE  | ENTER  | -      | =      | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_GRV, KC_MINS,  KC_EQL,  KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_BSLS, KC_LBRC, KC_RBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS },
-  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_HOME, KC_PGUP, KC_QUOT, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_END,  KC_PGDN, KC_DEL,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_SPC, TG(_FN), KC_SPC,  KC_SPC, KC_RALT, KC_RCTL,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
+  { KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_GRV, KC_MINS,  KC_EQL,  KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_TILD, KC_LBRC, KC_RBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS },
+  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_HOME, KC_PGUP, KC_ENT, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_END,  KC_PGDN, KC_DEL,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_UP },
+  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_SPC, TG(_QW), KC_SPC,  KC_SPC, KC_ENT, KC_MINS ,KC_EQL, KC_LEFT, KC_DOWN,   KC_RGHT },
  },
+
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -51,18 +52,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | CAPS   | PLAY   | NEXT   | STOP   |        |        | P4     | P5     | P6     | +      |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SHIFT  | MUTE   | VOL+   |  RESET |        |        | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * | SHIFT  | MUTE   | VOL+   |        |        |        | P1     | P2     | P3     | PENT   |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | CTRL   |    GUI |   ALT  | FN     |        |        | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_FN] = { /* FUNCTION */
-  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
-  { KC_MSEL, KC_CALC, KC_TRNS, KC_MAIL, KC_SPC, KC_SPC, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS },
-  { KC_CAPS, KC_MPLY, KC_MNXT, KC_MSTP, KC_SPC, KC_SPC, KC_P6,   KC_P5,   KC_P4,   KC_PLUS, KC_SPC,  KC_TRNS, KC_SPC,  KC_SPC,  KC_SPC },
-  { KC_LSFT, KC_MUTE, KC_VOLU, KC_APP,  KC_SPC, KC_SPC, KC_P3,   KC_P2,   KC_P1,   KC_TRNS, KC_SPC,  KC_SPC,  KC_SPC,  KC_UP, KC_TRNS },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_SPC, TG(_FN), KC_KP_0, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT },
+  { KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NLCK, RESET},
+  { KC_MSEL, KC_DEL , KC_UP,   BL_TOGG, RGB_TOG, KC_SPC, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_SPC,  KC_UP,  KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, RGB_MOD, KC_SPC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_LEFT, KC_DOWN, KC_RGHT,  KC_P8,  KC_P9 },
+  { KC_LSFT, KC_COPY,LCTL(KC_X),LCTL(KC_C),LCTL(KC_V), KC_PASTE, KC_P1,   KC_P2,   KC_P3,   KC_TRNS, KC_SPC,  KC_SPC,  KC_P4,  KC_P5, KC_P6 },
+  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_SPC, TG(_QW), KC_KP_0, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), KC_P1, KC_P2, KC_P3 },
  }
 };
 
