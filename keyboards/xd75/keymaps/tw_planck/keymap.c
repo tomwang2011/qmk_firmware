28e48f4b3e3573a21e53dd0d2bd212f8750b7ffb
tomwang2011 <tong.wang@liferay.com>
2018-05-11 14:18:56 -0700

    Update

--- keyboards/xd75/keymaps/tw_planck/keymap.c
+++ keyboards/xd75/keymaps/tw_planck/keymap.c
@@ -25,7 +25,9 @@
 #define REDO LSFT(LCTL(KC_Z))
 #define TERM LCA(KC_T)
 #define TTAB LSFT(LCTL(KC_T))
+#define TRST LSFT(LCTL(KC_R))
 #define TCPY LSFT(LCTL(KC_C))
+#define TPST LSFT(LCTL(KC_V))
 #define IJCL LCTL(KC_N)
 #define IJFL LSFT(LCTL(KC_N))
 
@@ -78,56 +80,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,    UNDO,    REDO,    KC_6,   KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TERM,      KC_LBRC, KC_RBRC, KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,     KC_BSLS },
-  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,   KC_PGUP, KC_ENT,  KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT },
-  { KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,    KC_PGDN, KC_DEL,  KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSPC },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  MO(_TG),   KC_SPC,  KC_EQL,  KC_ENT, KC_MINS, MO(_LW),  KC_LEFT, KC_DOWN,  KC_RGHT },
+  { KC_GESC, KC_1,    KC_2,       KC_3,       KC_4,       KC_5,     KC_GRV,    UNDO,      REDO,     KC_6,     KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC },
+  { KC_TAB,  KC_Q,    KC_W,       KC_E,       KC_R,       KC_T,     TERM,      KC_LBRC,   KC_RBRC,  KC_Y,     KC_U,     KC_I,       KC_O,     KC_P,     KC_BSLS },
+  { KC_LCTL, KC_A,    KC_S,       KC_D,       KC_F,       KC_G,     KC_HOME,   KC_PGUP,   KC_ENT,   KC_H,     KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT },
+  { KC_LSPO, KC_Z,    KC_X,       KC_C,       KC_V,       KC_B,     KC_END,    KC_PGDN,   KC_DEL,   KC_N,     KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSPC },
+  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    MO(_FN),     KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_MINS,    KC_LEFT,  KC_DOWN,  KC_RGHT },
  },
-
-
-/* FUNCTION
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    | NUM LK | RESET  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | DEL    | UP     | BL TOG | RGB TOG| SPACE  | PORTAL | TOMCAT | GITGUI | -      |        |  UP    |        | [      | ]      |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CAPS   | LEFT   | DOWN   | RGHT   |        | SPACE  | P4     | P5     | GITK   | +      |  LEFT  | Down   | RIGHT  |        | ENTER  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SHIFT  | COPY   | CTRLX  | CTRLC  | CTRLV  | PASTE  | P1     | P2     | P3     | PENT   |        |        |        |        | SFT    |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CTRL   |    GUI |   ALT  | FN     | SPACE  | SPACE  | QW     | P0     | P.     | FN     | ALT    | CTRL   |        |        |        |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
 
  [_FN] = { /* FUNCTION */
-  { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,  KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_TRNS,    KC_TRNS},
-  { KC_MSEL, KC_1,    KC_UP,       KC_3,       KC_4,       KC_5,   PORTAL,    TOMCAT,   GITGUI,   KC_6,     KC_7,     KC_8,      KC_9,     KC_0,     KC_RBRC},
-  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC, TTAB,      GITRBI,   GITK,     KC_4,     KC_5,     KC_6,      KC_PGUP,  KC_PGDN,  KC_DEL },
-  { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,   TCPY,      IJCL,     IJFL,     KC_1,     KC_2,     KC_3,      KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    MO(_FN),    KC_SPC,     KC_SPC, MO(_TG),   KC_SPC,   KC_EQL,   KC_ENT,   KC_MINS,  MO(_LW),   KC_LEFT,  KC_DOWN,  KC_RGHT},
+  { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
+  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITGUI,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
+  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITK,     KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
+  { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,     TCPY,      IJCL,      IJFL,     KC_1,     KC_2,     KC_3,       KC_HOME,  KC_UP,    KC_END },
+  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
- /* Lower
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    | NUM LK | RESET  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | DEL    | UP     | BL TOG | RGB TOG| SPACE  | P7     | P8     | P9     | -      |        |  UP    |        | [      | ]      |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CAPS   | LEFT   | DOWN   | RGHT   |        | SPACE  | P4     | P5     | P6     | +      |  LEFT  | Down   | RIGHT  |        | ENTER  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SHIFT  | COPY   | CTRLX  | CTRLC  | CTRLV  | PASTE  | P1     | P2     | P3     | PENT   |        |        |        |        | SFT    |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CTRL   |    GUI |   ALT  | FN     | SPACE  | SPACE  | QW     | P0     | P.     | FN     | ALT    | CTRL   |        |        |        |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
-
  [_LW] = { /* Lower */
-  { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,  KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_TRNS,    KC_TRNS},
-  { KC_MSEL, KC_EXLM, KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,PORTAL,    TOMCAT,   GITGUI,   KC_CIRC,  KC_AMPR,  KC_ASTR,   KC_LPRN,  KC_RPRN,  KC_TRNS},
-  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC, TTAB,      GITRBI,   GITK,     KC_4,     KC_5,     KC_6,      KC_PGUP,  KC_PGDN,  KC_DEL },
-  { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,   KC_1,      KC_2,     KC_3,     KC_1,     KC_2,     KC_3,      KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    MO(_FN),    KC_SPC,     KC_SPC, MO(_TG),   KC_SPC,   KC_EQL,   KC_ENT,   KC_MINS,  MO(_LW),   KC_LEFT,  KC_DOWN,  KC_RGHT},
+  { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
+  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITGUI,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
+  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITK,     KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
+  { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,     TRST,      TCPY,      TPST,     KC_1,     KC_2,     KC_3,       KC_HOME,  KC_UP,    KC_END },
+  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    MO(_FN),    KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
  /* Toggle
@@ -149,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_MSEL, KC_DEL , KC_UP,      BL_TOGG,    RGB_TOG,    KC_SPC, PORTAL,    TOMCAT,   GITGUI,   KC_7,     KC_8,     KC_9,     KC_TRNS,  KC_LBRC,  KC_RBRC},
   { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC, TTAB,      GITRBI,   GITK,     KC_4,     KC_5,     KC_6,     KC_PGUP,  KC_PGDN,  KC_DEL },
   { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,   TCPY,      IJCL,     IJFL,     KC_1,     KC_2,     KC_3,     KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    MO(_FN),    KC_SPC,     KC_SPC, MO(_TG),   KC_SPC,   KC_EQL,   KC_ENT,   KC_MINS,  MO(_LW),  KC_LEFT, KC_DOWN,  KC_RGHT},
+  { KC_LCTL, KC_LGUI, KC_LALT,    MO(_FN),    KC_SPC,    KC_SPC, MO(_TG),   KC_SPC,   KC_SPC,   KC_ENT,   MO(_LW),  KC_EQL,   KC_LEFT, KC_DOWN,  KC_RGHT},
  },
 };
 
