b0b3b355c3e658065fbdf5193c03c2beb54d707f
tomwang2011 <tong.wang@liferay.com>
2018-05-17 11:12:45 -0700

    Update

--- keyboards/xd75/keymaps/tw_planck/keymap.c
+++ keyboards/xd75/keymaps/tw_planck/keymap.c
@@ -36,26 +36,34 @@ enum custom_keycodes {
     TOMCAT,
     GITK,
     GITGUI,
-    GITRBI
+    GITRBI,
+    GITPSF,
+    GITCMT
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch(keycode) {
             case GITK:
-                SEND_STRING("gitk\n"); // this is our macro!
+                SEND_STRING("gitk"); // this is our macro!
                 return false;
             case GITGUI:
-                SEND_STRING("git gui\n"); // this is our macro!
+                SEND_STRING("git gui"); // this is our macro!
                 return false;
             case GITRBI:
-                SEND_STRING("git rebase -i HEAD~20\n"); // this is our macro!
+                SEND_STRING("git rebase -i HEAD~20"); // this is our macro!
                 return false;
             case PORTAL:
-                SEND_STRING("cd /liferaysc/liferay-portal\n"); // this is our macro!
+                SEND_STRING("cd /liferaysc/liferay-portal"); // this is our macro!
                 return false;
             case TOMCAT:
-                SEND_STRING("cd /home/tom/tomcat\n"); // this is our macro!
+                SEND_STRING("cd /home/tom/tomcat"); // this is our macro!
+                return false;
+            case GITPSF:
+                SEND_STRING("git push -f ");
+                return false;
+            case GITCMT:
+                SEND_STRING("git commit -m\"");
                 return false;
         }
     }
@@ -75,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | PG DN  | DEL    | N      | M      | ,      | .      | /      | UP     |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | tab    | SPACE  | FN     | ENTER  | -      | =      | LEFT   | DOWN   | RIGHT  |
+ * | HYPR   | LCTRL  | LGUI   | LALT   | FN     | SPACE  | TG     | SPACE  | SPACE  | ENTER  | LW     | -      | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -84,23 +92,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,       KC_E,       KC_R,       KC_T,     TERM,      KC_LBRC,   KC_RBRC,  KC_Y,     KC_U,     KC_I,       KC_O,     KC_P,     KC_BSLS },
   { KC_LCTL, KC_A,    KC_S,       KC_D,       KC_F,       KC_G,     KC_HOME,   KC_PGUP,   KC_ENT,   KC_H,     KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT },
   { KC_LSPO, KC_Z,    KC_X,       KC_C,       KC_V,       KC_B,     KC_END,    KC_PGDN,   KC_DEL,   KC_N,     KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSPC },
-  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    MO(_FN),     KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_MINS,    KC_LEFT,  KC_DOWN,  KC_RGHT },
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),    KC_SPC,   KC_SPC,    MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_MINS,    KC_LEFT,  KC_DOWN,  KC_RGHT },
  },
 
  [_FN] = { /* FUNCTION */
   { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
-  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITGUI,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
-  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITK,     KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
+  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITPSF,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
+  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITCMT,   KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
   { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,     TCPY,      IJCL,      IJFL,     KC_1,     KC_2,     KC_3,       KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    MO(_FN),    KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),    KC_SPC,   KC_SPC,    MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
  [_LW] = { /* Lower */
   { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
-  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITGUI,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
-  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITK,     KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
+  { KC_MSEL, KC_EXLM, KC_UP,      KC_HASH,    KC_DLR,     KC_PERC,  PORTAL,    TOMCAT,    GITPSF,   KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
+  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC,   TTAB,      GITRBI,    GITCMT,   KC_4,     KC_5,     KC_6,       KC_PGUP,  KC_PGDN,  KC_ENT },
   { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,     TRST,      TCPY,      TPST,     KC_1,     KC_2,     KC_3,       KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    KC_UNDS,    MO(_FN),    KC_SPC,   MO(_TG),   KC_SPC,    KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),    KC_SPC,   KC_SPC,    MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
  /* Toggle
@@ -119,10 +127,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_TG] = { /* Toggle */
   { KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,  KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,    KC_F12,   RESET,    KC_TRNS},
-  { KC_MSEL, KC_DEL , KC_UP,      BL_TOGG,    RGB_TOG,    KC_SPC, PORTAL,    TOMCAT,   GITGUI,   KC_7,     KC_8,     KC_9,     KC_TRNS,  KC_LBRC,  KC_RBRC},
-  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC, TTAB,      GITRBI,   GITK,     KC_4,     KC_5,     KC_6,     KC_PGUP,  KC_PGDN,  KC_DEL },
+  { KC_MSEL, KC_DEL , KC_UP,      BL_TOGG,    RGB_TOG,    KC_SPC, PORTAL,    TOMCAT,   GITPSF,   KC_7,     KC_8,     KC_9,     KC_TRNS,  KC_LBRC,  KC_RBRC},
+  { KC_CAPS, KC_LEFT, KC_DOWN,    KC_RGHT,    RGB_MOD,    KC_SPC, TTAB,      GITRBI,   GITCMT,   KC_4,     KC_5,     KC_6,     KC_PGUP,  KC_PGDN,  KC_DEL },
   { KC_LSFT, UNDO   , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), REDO,   TCPY,      IJCL,     IJFL,     KC_1,     KC_2,     KC_3,     KC_HOME,  KC_UP,    KC_END },
-  { KC_LCTL, KC_LGUI, KC_LALT,    MO(_FN),    KC_SPC,    KC_SPC, MO(_TG),   KC_SPC,   KC_SPC,   KC_ENT,   MO(_LW),  KC_EQL,   KC_LEFT, KC_DOWN,  KC_RGHT},
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),    KC_SPC, KC_SPC,    MO(_TG),  KC_SPC,   KC_ENT,   MO(_LW),  KC_DEL,   KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 };
 
