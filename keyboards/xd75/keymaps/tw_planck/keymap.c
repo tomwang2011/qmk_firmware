ee5f917586498d8ed5c3bde4886bf05b728af7c2
tomwang2011 <tong.wang@liferay.com>
2018-08-01 09:04:40 -0700

    Update

--- keyboards/xd75/keymaps/tw_planck/keymap.c
+++ keyboards/xd75/keymaps/tw_planck/keymap.c
@@ -31,6 +31,16 @@
 #define IJCL LCTL(KC_N)
 #define IJFL LSFT(LCTL(KC_N))
 
+#define MOUSEKEY_INTERVAL 16
+
+#define MOUSEKEY_DELAY 0
+
+#define MOUSEKEY_TIME_TO_MAX 60
+
+#define MOUSEKEY_MAX_SPEED 7
+
+#define MOUSEKEY_WHEEL_DELAY 0
+
 enum custom_keycodes {
     PORTAL = SAFE_RANGE,
     TOMCAT,
@@ -38,7 +48,9 @@ enum custom_keycodes {
     GITGUI,
     GITRBI,
     GITPSF,
-    GITCMT
+    GITCMT,
+    LRHM,
+    GWTI
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -64,6 +76,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             case GITCMT:
                 SEND_STRING("git commit -m\"");
+                return false;
+            case LRHM:
+                SEND_STRING("-Dliferay.home=/home/tom/tomcat");
+                return false;
+            case GWTI:
+                SEND_STRING("gw testIntegration --tests *.");
                 return false;
         }
     }
@@ -92,22 +110,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,       KC_E,       KC_R,      KC_T,        TERM,         KC_LBRC,   KC_RBRC,  KC_Y,     KC_U,     KC_I,       KC_O,     KC_P,     KC_BSLS },
   { KC_LCTL, KC_A,    KC_S,       KC_D,       KC_F,      KC_G,        LCTL(KC_T),   KC_PGUP,   KC_ENT,   KC_H,     KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT },
   { KC_LSPO, KC_Z,    KC_X,       KC_C,       KC_V,      KC_B,        LALT(KC_C),   KC_PGDN,   KC_DEL,   KC_N,     KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSPC },
-  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),   KC_SPC,      KC_SPC,       MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_MINS,    KC_LEFT,  KC_DOWN,  KC_RGHT },
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),   KC_SPC,      KC_SPC,       MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_UP,      KC_LEFT,  KC_DOWN,  KC_RGHT },
  },
 
  [_FN] = { /* FUNCTION */
   { KC_TRNS, KC_F1,   KC_F2,      KC_F3,      	KC_F4,     	KC_F5,       KC_F6,        KC_F7,     KC_F8,       KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
-  { KC_TRNS, KC_EXLM, KC_UP,      KC_HASH,    	KC_DLR,    	KC_PERC,     PORTAL,       TOMCAT,    GITPSF,      KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
-  { KC_TRNS, KC_LEFT, KC_DOWN,    KC_RGHT,    	KC_TRNS,   	KC_TRNS,     TTAB,         GITRBI,    GITCMT,      KC_LEFT,  KC_DOWN,  KC_UP,      KC_RGHT,  KC_PGDN,  KC_ENT },
-  { KC_TRNS, UNDO   , LCTL(KC_X), TCPY,			    TPST,		    LCTL(KC_T),  LALT(KC_C),   IJCL,      IJFL,        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_HOME,  KC_UP,    KC_END },
+  { KC_TRNS, KC_EXLM, KC_UP,      KC_HASH,    	KC_DLR,    	KC_PERC,     PORTAL,       TOMCAT,    LRHM,        KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
+  { KC_TRNS, KC_LEFT, KC_DOWN,    KC_RGHT,    	KC_TRNS,   	KC_TRNS,     TTAB,         GWTI,      GITCMT,      KC_LEFT,  KC_DOWN,  KC_UP,      KC_RGHT,  KC_PGDN,  KC_ENT },
+  { KC_TRNS, UNDO   , LCTL(KC_X), TCPY,			TPST,		LCTL(KC_T),  LALT(KC_C),   IJCL,      IJFL,        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_HOME,  KC_UP,    KC_END},
   { KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    	MO(_FN),   	KC_SPC,      LCTL(KC_MINS),MO(_TG),   LCTL(KC_EQL),KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
  [_LW] = { /* Lower */
   { KC_TRNS, KC_F1,   KC_F2,      KC_F3,      	KC_F4,     	KC_F5,       KC_F6,        KC_F7,     KC_F8,       KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_TRNS,  KC_TRNS},
   { KC_TRNS, KC_EXLM, KC_UP,      KC_HASH,    	KC_DLR,    	KC_PERC,     PORTAL,       TOMCAT,    GITPSF,      KC_CIRC,  KC_AMPR,  KC_ASTR,    KC_MINS,  KC_EQL,   KC_TRNS},
-  { KC_TRNS, KC_LEFT, KC_DOWN,    KC_RGHT,    	KC_TRNS,   	KC_TRNS,     TTAB,         GITRBI,    GITCMT,      KC_MS_L,  KC_MS_D,  KC_MS_U,    KC_MS_R,  KC_PGDN,  KC_ENT },
-  { KC_TRNS, UNDO   , LCTL(KC_X), TCPY, 		    TPST,		    LCTL(KC_T),  TRST,         TCPY,      TPST,        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_HOME,  KC_UP,    KC_END },
+  { KC_TRNS, KC_LEFT, KC_DOWN,    KC_RGHT,    	KC_TRNS,   	KC_TRNS,     TTAB,         GITRBI,    GITCMT,      KC_LEFT,  KC_DOWN,  KC_UP,      KC_RGHT,  KC_PGDN,  KC_ENT },
+  { KC_TRNS, UNDO   , LCTL(KC_X), TCPY,			TPST,		LCTL(KC_T),  TRST,         TCPY,      TPST,        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_HOME,  KC_UP,    KC_END },
   { KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    	MO(_FN),   	KC_SPC,      LCTL(KC_MINS),MO(_TG),   LCTL(KC_EQL),KC_ENT,   MO(_LW),  KC_DEL,     KC_LEFT,  KC_DOWN,  KC_RGHT},
  },
 
