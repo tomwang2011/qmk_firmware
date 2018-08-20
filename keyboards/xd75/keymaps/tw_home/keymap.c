0065ea60201b771cc53d8e4287817b045d32059a
tom <tomwang2011@gmail.com>
2018-08-19 18:18:02 -0700

    Update xd75

--- keyboards/xd75/keymaps/tw_home/keymap.c
+++ keyboards/xd75/keymaps/tw_home/keymap.c
@@ -109,8 +109,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_GESC, KC_1,    KC_2,       KC_3,       KC_4,      KC_5,        KC_GRV,       UNDO,      REDO,     KC_6,     KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC },
   { KC_TAB,  KC_Q,    KC_W,       KC_E,       KC_R,      KC_T,        TERM,         KC_LBRC,   KC_RBRC,  KC_Y,     KC_U,     KC_I,       KC_O,     KC_P,     KC_BSLS },
   { KC_LCTL, KC_A,    KC_S,       KC_D,       KC_F,      KC_G,        KC_PLUS,      KC_PGUP,   KC_ENT,   KC_H,     KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT },
-  { KC_LSPO, KC_Z,    KC_X,       KC_C,       KC_V,      KC_B,        KC_MINS,      KC_PGDN,   KC_DEL,   KC_N,     KC_M,     KC_COMM,    KC_DOT,   KC_UP,  KC_RSPC },
-  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),   KC_SPC,      KC_SPC,       MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_SLSH,    KC_LEFT,  KC_DOWN,  KC_RGHT },
+  { KC_LSPO, KC_Z,    KC_X,       KC_C,       KC_V,      KC_B,        KC_MINS,      KC_PGDN,   KC_DEL,   KC_N,     KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSPC },
+  { KC_HYPR, KC_LCTL, KC_LGUI,    KC_LALT,    MO(_FN),   KC_SPC,      KC_SPC,       MO(_TG),   KC_SPC,   KC_ENT,   MO(_LW),  KC_UP,    KC_LEFT,  KC_DOWN,  KC_RGHT },
  },
 
  [_FN] = { /* FUNCTION */
