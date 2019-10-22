203f1214ca19c7d89ff4d3a7b2e2c9b0bb161cc4
Tom Wang <tomwang@Toms-MacBook-Pro.local>
2019-10-21 22:05:38 -0700

    Updates to ergodox

--- keyboards/ergodox_ez/keymaps/tw/keymap.c
+++ keyboards/ergodox_ez/keymaps/tw/keymap.c
@@ -51,20 +51,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
         KC_LCTL,        KC_LALT,      KC_LGUI,  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_MINS,
+                                              LGUI(KC_SPC),  LALT(KC_ENT),
                                                               KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                               KC_SPC,KC_TAB,KC_END,
         // right hand
              KC_TILD,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             TT(MDIA),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
+             KC_EQL,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
                                   KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TT(SYMB),
-             KC_EQL,  CTL_T(KC_ESC),
+             TT(MDIA),  LCTL(KC_S),
              KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+             KC_PGDN,KC_UNDS, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
+       RGB_TOG  , KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
